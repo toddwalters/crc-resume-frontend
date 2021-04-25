c3753dcba96299af6b7b9c2c336f473d3c414f7a
toddwalters <waltodders@gmail.com>
2021-04-25 19:43:52 -0400

    initializing repo

---
+++ src/js/visitors.js
@@ -0,0 +1,14 @@
+
+const url='https://api.dev.resume.waltodders.com/dev/counter';
+
+var count = " ";
+
+async function getCount(){
+    let response = await fetch(url);
+    let data = await response.json()
+    let num = data.Attributes.VisitCount;
+    document.getElementById("visits").innerHTML = count.concat(num);
+}
+
+getCount()
+    .then(data => console.log(data))
